4f4dc5e59f4c40bfe4f54cc148ea8feeed2c2eaf
BudgetCOntrol <140796512+BudgetControl@users.noreply.github.com>
2024-09-09 13:42:50 +0200

    Fix planned entry model added fillable fields

--- libs/Model/PlannedEntry.php
+++ libs/Model/PlannedEntry.php
@@ -11,4 +11,23 @@ class PlannedEntry extends Entry implements EntryInterface
 
     protected $hidden = ['id'];
 
+    protected $fillable = [
+        'date_time',
+        'updated_at',
+        'created_at',
+        'uuid',
+        'amount',
+        'note',
+        'type',
+        'category_id',
+        'account_id',
+        'transfer_id',
+        'transfer_relation',
+        'currency_id',
+        'payment_type',
+        'payee_id',
+        'deleted_at',
+        'workspace_id',
+        'planning'
+    ];
 }
