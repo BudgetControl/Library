f3a4d7622d8d2c0117e7d46f1c4e3af975df3490
BudgetControl <140796512+BudgetControl@users.noreply.github.com>
2024-12-17 16:12:32 +0100

    Update Entry.php

--- libs/Entity/Entry.php
+++ libs/Entity/Entry.php
@@ -12,13 +12,15 @@ enum Entry: string {
     case incoming   = 'incoming';
     case transfer   = 'transfer';
     case debit      = 'debit';
+    case saving      = 'saving';
 
     public static function types(): array {
         return [
             self::expenses->value,
             self::incoming->value,
             self::transfer->value,
-            self::debit->value
+            self::debit->value,
+            self::saving->value
         ];
     }
 }
